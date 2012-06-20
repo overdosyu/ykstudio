2617075147adb2554901c7ef2281a7716c689155
Simon Yu <syu@Simon-Yus-MacBook-Pro.local>
2012-06-20 16:59:55 -0700

    * Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ykstudio::Application.config.secret_token = 'd7d140b615a74b42eae596371e3e7a9305990c7811b9bdba6b7fbe77981b182ca04c17bc20f1d9ed511e0aaedda8996ead24392879f9fe2b18f4433a48a33d87'
